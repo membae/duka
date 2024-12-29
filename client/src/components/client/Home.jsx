c63af62191a5af174399c7d61cf08dfb92932f47
Philip <membaphilip1@gmail.com>
2024-12-29 21:46:18 +0300

    home

---
+++ client/src/components/client/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+       <h1>Happy shopping</h1> 
+       <div>
+        <div>
+            <img src="/Home1.jpg" alt="" />
+        </div>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quibusdam qui atque libero, corrupti nostrum possimus eaque dicta nam, cum iure! Maxime sit dicta non iure doloribus fuga ea sapiente.</p>
+        </div>
+       </div>
+    </div>
+  )
+}
+
+export default Home
