6965c52468806ee611542f055f62c65e64a526f8
Philip <membaphilip1@gmail.com>
2024-12-30 17:33:44 +0300

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import Home from './components/client/Home'
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div>
      <Router>
      <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/navbar' element={<Navbar/>}></Route>
       </Routes>
      </Router>
     </div>
