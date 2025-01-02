19489e6d535b4df752e643e768ed696a82e723d5
memba <membaphilip1@gmail.com>
2025-01-02 21:15:13 +0300

    started working on the cart

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,18 @@ import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
 import Home from './components/client/Home'
 import Navbar from './components/Navbar'
 import ProductCard from './components/client/ProductCard'
-import ProductProvider from './components/ProductContext'
+import Cart from './components/client/Cart'
+
 
 function App() {
   return (
    
-       <Router>
+    <Router>
      <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/navbar' element={<Navbar/>}></Route>
         <Route path="/products" element={<ProductCard/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
       </Routes>
      </Router>
    
